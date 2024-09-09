e4cbc993b02ebecfccd57cbd54aa664a0e1a4701
bryce72 <brycedunlapsf56@gmail.com>
2024-09-09 16:20:56 -0400

    first fix

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React -edited by Bryce ---again
+          Learn React -edited by Bryce
+        </a>
       </header>
     </div>
   );
