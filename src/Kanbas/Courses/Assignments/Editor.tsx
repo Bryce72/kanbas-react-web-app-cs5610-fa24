12c7d61ab96141a3520e513bb24c23af9e2aee35
Bryce72 <brycedunlapsf56@gmail.com>
2024-09-12 12:09:01 -0400

    on 3.8

---
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -0,0 +1,69 @@
+export default function AssignmentEditor() {
+    return (
+      <div id="wd-assignments-editor">
+        <label htmlFor="wd-name"><b>Assignment Name</b></label><br /> <br />
+        <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
+        <textarea id="wd-description">The assignment is available online Submit a link to the landing page of</textarea>
+        <br />
+        <table>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-points">Points</label>
+            </td>
+            <td>
+              <input id="wd-points" value={100} />
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-group">Assignment Group</label>
+            </td>
+            <td>
+              <select id="wd-group">
+              <option value="ASSIGNMENTS">ASSIGNMENTS</option>
+              </select>
+              {/* Add more to this drop down when I receive more info on what is needed */}
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-display-grade-as">Display Grade as</label>
+            </td>
+            <td>
+              <select id="wd-display-grade-as">
+              <option value="Percentage">Percentage</option>
+              </select>
+              {/* Add more to this drop down when I receive more info on what is needed */}
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-submission-type">Submission Type</label>
+            </td>
+            <td>
+              <select id="wd-submission-type">
+              <option value="Online">Online</option>
+              </select>
+              {/* Add more to this drop down when I receive more info on what is needed */}
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+                <label>Online Entry Options</label> <br /> {/* There was no associated tag with the label for this one. Professor error? */}
+                
+                <input type="checkbox" id="wd-text-entry"/>
+                <label htmlFor="wd-text-entry">Text Entry</label><br/>
+
+                <input type="checkbox" id="wd-website-url"/>
+                <label htmlFor="wd-website-url">Website URL</label><br/>
+
+                <input type="checkbox" id="wd-media-recordings"/>
+                <label htmlFor="wd-media-recordings">Media Recordings</label><br/>
+                
+            </td>
+          </tr>
+          {/* Complete on your own */}
+        </table>
+      </div>
+  );}
+  
