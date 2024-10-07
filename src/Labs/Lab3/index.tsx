03e1aa2cad76785c2ebb6745ea98b55ae93d5c54
Bryce72 <brycedunlapsf56@gmail.com>
2024-10-07 12:07:17 -0400

    onto 2.3.3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,10 +1,27 @@
 import VariablesAndConstants from "./VariablesAndConstants";
-
+import VariableTypes from "./VariableTypes";
+import BooleanVariable from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
 export default function Lab3() {
   return(
     <div id="wd-lab3">
       <h3>Lab 3</h3>
       <VariablesAndConstants/>
+      <VariableTypes />
+      <BooleanVariable />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
     </div>
   );
 }
