0010c552d41575c19614b6776a86864af5f1150e
Bryce72 <brycedunlapsf56@gmail.com>
2024-10-18 16:24:46 -0400

    updated packages. check section 2.4 on a4

--- src/Labs/Lab4/ArrayStateVariable.tsx
+++ src/Labs/Lab4/ArrayStateVariable.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import './ArrayStateVariable.css'
 export default function ArrayStateVariable() {
   const [array, setArray] = useState([1, 2, 3, 4, 5]);
   const addElement = () => {
